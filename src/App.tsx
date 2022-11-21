a15e1af8494c115f5d36dbd9787f93420df5dbd5
Camila-Virtus <9401Mil@>
2022-11-21 20:29:23 -0300

    added initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
 const App = () => {
   return (
-    <div className="App">
-      Oi
-    </div>
+    <BrowserRouter>
+    <AppRoutes/>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+
+const AppRoutes = () => {
+    return(
+      <Routes>
+        <Route path='/home' element={<p>Página Inicial</p>}></Route>
+        <Route path='*' element={<Navigate to="/home"/>}></Route>
+      </Routes>
+    )
+  }
+
+  export default AppRoutes
