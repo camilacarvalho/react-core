24bd8e941437ed81bb9f4643b3762e34f8eca0d4
Camila-Virtus <9401Mil@>
2022-11-21 20:38:31 -0300

    added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes";
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes';
 const App = () => {
-  return (
-    <BrowserRouter>
-    <AppRoutes/>
-    </BrowserRouter>
+    return (
+        <BrowserRouter>
+            <AppRoutes/>
+        </BrowserRouter>
     
-  );
-}
+    );
+};
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-
+import { Navigate, Route, Routes } from 'react-router-dom';
 const AppRoutes = () => {
     return(
-      <Routes>
-        <Route path='/home' element={<p>Página Inicial</p>}></Route>
-        <Route path='*' element={<Navigate to="/home"/>}></Route>
-      </Routes>
-    )
-  }
-
-  export default AppRoutes
+        <Routes>
+            <Route path='/home' element={<p>Página Inicial</p>}></Route>
+            <Route path='*' element={<Navigate to="/home"/>}></Route>
+        </Routes>
+    );
+};
+export default AppRoutes;
