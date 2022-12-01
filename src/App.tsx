c574bbe86af243921a875f489f8288243595c973
Camila-Virtus <9401Mil@>
2022-12-01 09:43:26 -0300

    Addeding and configuring react query

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,25 @@ import { Login, Sidebar } from './components';
 import { AppSidebarProvider } from './contexts';
 import { LoginProvider } from './contexts/LoginContext';
 import AppRoutes from './routes';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
 const App = () => {
     return (
-        <LoginProvider>
-            <Login>
-                <BrowserRouter>
-                    <AppSidebarProvider>
-                        <Sidebar>
-                            <AppRoutes />
-                        </Sidebar>
-                    </AppSidebarProvider>
-                </BrowserRouter>
-            </Login>
-        </LoginProvider>
+        <QueryClientProvider client={queryClient}>
+            <LoginProvider>
+                <Login>
+                    <BrowserRouter>
+                        <AppSidebarProvider>
+                            <Sidebar>
+                                <AppRoutes />
+                            </Sidebar>
+                        </AppSidebarProvider>
+                    </BrowserRouter>
+                </Login>
+            </LoginProvider>
+        </QueryClientProvider>
     );
 };
 
