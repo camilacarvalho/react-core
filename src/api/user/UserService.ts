904df5e88f90b9a88ce1e40ca52a3a5140d69f0b
Camila-Virtus <9401Mil@>
2022-12-12 21:36:20 -0300

    changed user service

--- src/api/user/UserService.ts
+++ src/api/user/UserService.ts
@@ -1,23 +1,25 @@
 import { userData } from '../../models';
 import Api from '../axios';
 
-const getAll = async(): Promise<userData[]> => {
+const getAll = async (): Promise<userData[]> => {
     const { data } = await Api.get<userData[]>('/users');
     return data;
 };
 
-const getById = async(id:number): Promise<userData> => {
-    return await Api.get(`/users${id}`);
-};
-const create = async(user: userData): Promise<userData> => {
-    const { data } = await Api.post('/users', user);   
+const getById = async (id: number | string): Promise<userData> => {
+    const{ data } = await Api.get(`/users/${id}`);
     return data;
 };
-const updateById = async(id:number, user: userData): Promise<void> => {
-    await Api.put(`/users${id}`, user);
+const create = async (user: userData): Promise<userData> => {
+    const { data } = await Api.post('/users', user);
+    return data;
 };
-const deleteById = async(id:number): Promise<void> => {
-    await Api.delete(`/users${id}`);
+const updateById = async (user: userData): Promise<userData> => {
+    const{ data } = await Api.put(`/users/${user.id}`, user);
+    return data;
+};
+const deleteById = async (id: number): Promise<void> => {
+    return await Api.delete(`/users/${id}`);
 };
 export const UserService = {
     getAll, getById, create, updateById, deleteById
