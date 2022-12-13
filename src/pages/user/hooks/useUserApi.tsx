fd9ac25ced46d82fcbad1cf67eab6f67ea630aff
Camila-Virtus <9401Mil@>
2022-12-12 21:40:10 -0300

    altered user api

--- src/pages/user/hooks/useUserApi.tsx
+++ src/pages/user/hooks/useUserApi.tsx
@@ -1,17 +1,24 @@
-import { QueryClient, useQuery, useMutation } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { UserService } from '../../../api/user/UserService';
 import notification from '../../../utils/notification';
 
-
-
 const userApi = () => {
-    const queryClient = new QueryClient();
+
+    const queryClient = useQueryClient();
 
     const getAll = () => {
         return useQuery('users', () => { return UserService.getAll(); }, {
             onError: (error: any) => {
                 notification('error', error.message ?? 'Não foi possível carregar lista de usuários', 'toast-user-list-error');
             }
+        });
+    };
+
+    const getById = (id: number | string) => {
+        return useQuery(['userComponent', id], () => { return UserService.getById(id); }, {
+            onError: (error: any) => {
+                notification('error', error.message ?? 'Não foi possível carregar o usuário', 'toast-user-error');
+            },
         });
     };
 
@@ -27,10 +34,37 @@ const userApi = () => {
         });
     };
 
+    const update = (id: number | string) => {
+        return useMutation(UserService.updateById, {
+            onSuccess: () => {
+                queryClient.invalidateQueries(['userComponent', id]);
+                queryClient.invalidateQueries('users');
+                notification('success','Usuário atualizado com sucesso', 'toast-user-update-success');
+            }, onError: (error: any) => {
+                notification('error', error?.message??'Erro ao atualizar usuário', 'toast-user-update-error');
+            }
+        });
+    };
+
+    const deleteById = () => {
+        return useMutation(UserService.deleteById, {
+            onSuccess: () => {
+                queryClient.invalidateQueries('users');
+                notification('success','Usuário removido com sucesso', 'toast-user-remove-success');
+            },
+            onError: (error: any) => {
+                notification('error', error?.message??'Erro ao remover usuário', 'toast-user-remove-error');
+            }
+        });
+    };
+
     return {
         user: {
             create,
-            getAll
+            getAll,
+            update,
+            getById,
+            deleteById,
         }
     };
 };
